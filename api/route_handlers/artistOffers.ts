ed8f00b3becd06c335627abbda2a50d7a3b9b7d8
Nate Fritts <29683141+nate-fritts@users.noreply.github.com>
2026-09-24 19:31:55 -0400

    Mock ArtistOffer routes

---
+++ api/route_handlers/artistOffers.ts
@@ -0,0 +1,7 @@
+export const postArtistOffer = async () => {};
+
+export const getArtistOfferById = async () => {};
+
+export const patchArtistOfferById = async () => {};
+
+export const deleteArtistOfferById = async () => {};
---
+++ api/routers/artists.ts
@@ -0,0 +1,19 @@
+import { setArtistVar } from "@/middleware.ts";
+import { Hono } from "@hono";
+export const ArtistRoutes = new Hono();
+
+// ARTISTS
+import { postArtist, getArtistById, deleteArtistById, patchArtistById } from "@/route_handlers/artists.ts";
+import { getArtistOfferById, postArtistOffer, patchArtistOfferById, deleteArtistOfferById } from "@/route_handlers/artistOffers.ts";
+
+ArtistRoutes.post('/', postArtist);
+
+ArtistRoutes.use('/:artistId', setArtistVar);
+ArtistRoutes.get('/:artistId', getArtistById);
+ArtistRoutes.patch('/:artistId', patchArtistById);
+ArtistRoutes.delete('/:artistId', deleteArtistById);
+
+ArtistRoutes.post('/:artistId/offers', postArtistOffer);
+ArtistRoutes.get('/:artistId/offers/:offerId', getArtistOfferById);
+ArtistRoutes.patch('/:artistId/offers/:offerId', patchArtistOfferById);
+ArtistRoutes.delete('/:artistId/offers/:offerId', deleteArtistOfferById);
